9efc556de831f05896f9d94589a40ca12c00faa5
Deepanshu7517 <deepudagar90@gmail.com>
2025-11-05 14:48:37 +0530

    first commit

--- src/app/(home)/_components/services.tsx
+++ src/app/(home)/_components/services.tsx
@@ -8,8 +8,30 @@ import {
 import { Section, SectionHeading } from "../../../components/site/section";
 import { services } from "../../../lib/data";
 import { Button } from "../../../components/ui/button";
-import { motion } from "framer-motion";
+import { motion, type Variants } from "framer-motion";
 export default function Services() {
+  const container: Variants = {
+    hidden: { opacity: 1 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2, // delay between child animations
+        delayChildren: 0.3, // optional
+      },
+    },
+  };
+
+  const itemMotion: Variants = {
+    hidden: { scale: 0, opacity: 0 },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        duration: 0,
+        ease: "easeOut" as any, // 👈 TypeScript fix
+      },
+    },
+  };
   return (
     <Section id="services">
       <motion.div
@@ -19,12 +41,15 @@ export default function Services() {
       >
         <SectionHeading title="What We Do Best" subtitle="Our Services" />
       </motion.div>
-      <motion.div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+      >
         {services.map((service, index) => (
           <motion.div
-            whileInView={{ scale: 1 , opacity:1}}
-            initial={{ scale: 0, opacity:0 }}
-            transition={{ duration: 0.3, delay:index * 0.2, ease:"linear"}}
+            variants={itemMotion}
             key={index}
             className="relative rounded-2xl p-[2px] bg-transparent transition-all
              before:absolute before:inset-0 before:rounded-2xl 
